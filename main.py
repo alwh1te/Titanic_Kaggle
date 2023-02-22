dad6f167d927edc4acc57600a1f2bdbf6ac456b3
DedINSIDE <filatovalex775@gmail.com>
2023-02-22 22:16:51 +0500

    Complete)))

--- main.py
+++ main.py
@@ -47,6 +47,19 @@ for dataset in train_test_data:
     dataset['Embarked'] = dataset['Embarked'].map( {'S': 0, 'C': 1, 'Q': 2} ).astype(int)
 
 for dataset in train_test_data:
+    age_avg = dataset['Age'].mean()
+    age_std = dataset['Age'].std()
+    age_null_count = dataset['Age'].isnull().sum()
+
+    age_null_random_list = np.random.randint(age_avg - age_std, age_avg + age_std, size=age_null_count)
+    dataset['Age'][np.isnan(dataset['Age'])] = age_null_random_list
+    dataset['Age'] = dataset['Age'].astype(int)
+
+train['AgeBand'] = pd.cut(train['Age'], 5)
+
+print(train[['AgeBand', 'Survived']].groupby(['AgeBand'], as_index=False).mean())
+
+for dataset in train_test_data:
     dataset.loc[dataset['Age'] <= 16, 'Age'] = 0
     dataset.loc[(dataset['Age'] > 16) & (dataset['Age'] <= 32), 'Age'] = 1
     dataset.loc[(dataset['Age'] > 32) & (dataset['Age'] <= 48), 'Age'] = 2
@@ -91,10 +104,10 @@ y_train = train['Survived']
 
 X_test = test.drop("PassengerId", axis=1).copy()
 
-X_train.shape, y_train.shape, X_test.shape
 
 X_train = X_train.drop('FareBand', axis=1)
-
+X_train = X_train.drop('AgeBand', axis=1)
+X_train.info()
 # Importing Classifier Modules
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC, LinearSVC
@@ -104,4 +117,75 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import Perceptron
 from sklearn.linear_model import SGDClassifier
+from sklearn.ensemble import VotingClassifier
 
+clf = LogisticRegression()
+clf.fit(X_train, y_train)
+y_pred_log_reg = clf.predict(X_test)
+acc_log_reg = round( clf.score(X_train, y_train) * 100, 2)
+print (str(acc_log_reg) + ' percent')
+
+clf = SVC()
+clf.fit(X_train, y_train)
+y_pred_svc = clf.predict(X_test)
+acc_svc = round(clf.score(X_train, y_train) * 100, 2)
+print (acc_svc)
+
+clf = LinearSVC()
+clf.fit(X_train, y_train)
+y_pred_linear_svc = clf.predict(X_test)
+acc_linear_svc = round(clf.score(X_train, y_train) * 100, 2)
+print (acc_linear_svc)
+
+clf = KNeighborsClassifier(n_neighbors = 3)
+clf.fit(X_train, y_train)
+y_pred_knn = clf.predict(X_test)
+acc_knn = round(clf.score(X_train, y_train) * 100, 2)
+print (acc_knn)
+
+clf = DecisionTreeClassifier()
+clf.fit(X_train, y_train)
+y_pred_decision_tree = clf.predict(X_test)
+acc_decision_tree = round(clf.score(X_train, y_train) * 100, 2)
+print (acc_decision_tree)
+
+clf = RandomForestClassifier(n_estimators=100)
+clf.fit(X_train, y_train)
+y_pred_random_forest = clf.predict(X_test)
+acc_random_forest = round(clf.score(X_train, y_train) * 100, 2)
+print (acc_random_forest)
+
+clf = GaussianNB()
+clf.fit(X_train, y_train)
+y_pred_gnb = clf.predict(X_test)
+acc_gnb = round(clf.score(X_train, y_train) * 100, 2)
+print (acc_gnb)
+
+clf = Perceptron(max_iter=5, tol=None)
+clf.fit(X_train, y_train)
+y_pred_perceptron = clf.predict(X_test)
+acc_perceptron = round(clf.score(X_train, y_train) * 100, 2)
+print (acc_perceptron)
+
+clf = SGDClassifier(max_iter=5, tol=None)
+clf.fit(X_train, y_train)
+y_pred_sgd = clf.predict(X_test)
+acc_sgd = round(clf.score(X_train, y_train) * 100, 2)
+print (acc_sgd)
+
+models = pd.DataFrame({
+    'Model': ['Logistic Regression', 'Support Vector Machines', 'Linear SVC',
+              'KNN', 'Decision Tree', 'Random Forest', 'Naive Bayes',
+              'Perceptron', 'Stochastic Gradient Decent'],
+
+    'Score': [acc_log_reg, acc_svc, acc_linear_svc,
+              acc_knn, acc_decision_tree, acc_random_forest, acc_gnb,
+              acc_perceptron, acc_sgd]
+})
+
+models.sort_values(by='Score', ascending=False)
+
+submission = pd.DataFrame({
+        "PassengerId": test["PassengerId"],
+        "Survived": y_pred_random_forest
+    })
